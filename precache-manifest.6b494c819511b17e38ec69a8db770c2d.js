b28a3e62743227cd7e1f2e21d89ae4d390ef938e
Jinri <54436297+zzznly@users.noreply.github.com>
2022-01-14 00:19:32 +0900

    Add files via upload

---
+++ precache-manifest.6b494c819511b17e38ec69a8db770c2d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c729088548b47e3cb822",
+    "url": "/yusinsa/css/app.0342756e.css"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/yusinsa/favicon.ico"
+  },
+  {
+    "revision": "cf43a1f1f02796499c284945949347f1",
+    "url": "/yusinsa/img/heart_empty.cf43a1f1.svg"
+  },
+  {
+    "revision": "f913b3eae02f32f8f90f9a1d391085cb",
+    "url": "/yusinsa/img/heart_filled.f913b3ea.svg"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/yusinsa/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/yusinsa/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/yusinsa/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/yusinsa/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/yusinsa/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/yusinsa/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/yusinsa/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/yusinsa/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/yusinsa/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/yusinsa/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/yusinsa/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/yusinsa/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/yusinsa/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/yusinsa/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/yusinsa/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "30bff1590f2ad83e3cc8eec3e4e2ec71",
+    "url": "/yusinsa/img/img_random.30bff159.png"
+  },
+  {
+    "revision": "3f89b1419216f52145ff539aabc8cd56",
+    "url": "/yusinsa/img/random.3f89b141.svg"
+  },
+  {
+    "revision": "c729088548b47e3cb822",
+    "url": "/yusinsa/js/app.6d51f7c2.js"
+  },
+  {
+    "revision": "bc879a8d77efac85d1bb",
+    "url": "/yusinsa/js/chunk-vendors.2b580d05.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/yusinsa/robots.txt"
+  }
+]);
